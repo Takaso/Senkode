d2fed9e608d6ae4d5811e6bcadcbe0765af7e988
Takaso <70639611+Takaso@users.noreply.github.com>
2022-08-26 21:47:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import base64;
-#senkode: by takaso and loja
+
 def key(seed:int=0x80) -> str:
     def gcd(seed:int, b:int) -> int:
         if b == 0:
@@ -12,9 +12,9 @@ def encrypt(string:str, seed:int) -> str:
     return base64.b16encode(bytes(sex, encoding="utf-8"));
 
 def decrypt(string:str, seed:int) -> str:
-    s = base64.b16decode(string)
-    a = "".join([chr(int(ord(i)^seed)) for i in list(str(s))])
-    return a.split("$")[1]
+    s = base64.b16decode(string);
+    a = "".join([chr(int(ord(i)^seed)) for i in list(str(s))]);
+    return a.split("$")[1];
         
 print(encrypt("Loja", key(69))); #=>4F6C6962
-print(decrypt("4F6C6962", key(69)))
+print(decrypt("4F6C6962", key(69)));
