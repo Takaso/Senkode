d11af9734086d7a2dae6f13b250b677eaa525833
Komodo <loja@duck.com>
2022-09-04 12:04:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import base64;
+import base64, sys;
 
 def key(seed:int=0x80) -> str:
     def gcd(seed:int, b:int) -> int:
@@ -14,7 +14,11 @@ def encrypt(string:str, seed:int) -> str:
 def decrypt(string:str, seed:int) -> str:
     s = base64.b16decode(string);
     a = "".join([chr(int(ord(i)^seed)) for i in list(str(s))]);
-    return a.split("$")[1];
-        
-print(encrypt("Loja", key(69))); #=>4F6C6962
-print(decrypt("4F6C6962", key(69)));
+    return a#.split("$")[1];
+
+try:
+    if sys.argv[1].startswith("-e") and sys.argv[3].startswith("-k"): print(encrypt(sys.argv[2], key(int(sys.argv[4]))));
+    elif sys.argv[1].startswith("-d") and sys.argv[3].startswith("-k"): print(decrypt(sys.argv[2], key(int(sys.argv[4]))));
+    else: print("nigger");
+except Exception as e:
+    print(e);
